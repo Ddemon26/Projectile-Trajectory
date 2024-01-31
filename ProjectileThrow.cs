4a0b598b38bd61d7fac1282bd8797d585cd6a989
Ddemon26 <95268795+Ddemon26@users.noreply.github.com>
2024-01-30 20:24:09 -0800

    Update ProjectileThrow.cs

--- ProjectileThrow.cs
+++ ProjectileThrow.cs
@@ -1,1 +1,75 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(TrajectoryPredictor))]
+public class ProjectileThrow : MonoBehaviour
+{
+    [Header("Projectile Settings")]
+    [SerializeField]
+    private Rigidbody objectToThrow;
+
+    [SerializeField, Range(0.0f, 50.0f)]
+    private float throwForce;
+
+    [SerializeField]
+    private Transform startPosition;
+
+    [Header("Input Settings")]
+    public InputAction fireAction;
+
+    private TrajectoryPredictor trajectoryPredictor;
+
+    private void Awake()
+    {
+        trajectoryPredictor = GetComponent<TrajectoryPredictor>();
+        ValidateComponents();
+    }
+
+    private void OnEnable()
+    {
+        fireAction.Enable();
+        fireAction.performed += ThrowObject;
+    }
+
+    private void OnDisable()
+    {
+        fireAction.Disable();
+        fireAction.performed -= ThrowObject;
+    }
+
+    private void Update()
+    {
+        PredictProjectileTrajectory();
+    }
+
+    private void PredictProjectileTrajectory()
+    {
+        trajectoryPredictor.PredictTrajectory(GetProjectileProperties());
+    }
+
+    private ProjectileProperties GetProjectileProperties()
+    {
+        return new ProjectileProperties
+        {
+            direction = startPosition.forward,
+            initialPosition = startPosition.position,
+            initialSpeed = throwForce,
+            mass = objectToThrow.mass,
+            drag = objectToThrow.drag
+        };
+    }
+
+    private void ThrowObject(InputAction.CallbackContext context)
+    {
+        Rigidbody thrownObject = Instantiate(objectToThrow, startPosition.position, Quaternion.identity);
+        thrownObject.AddForce(startPosition.forward * throwForce, ForceMode.Impulse);
+    }
+
+    private void ValidateComponents()
+    {
+        if (startPosition == null)
+        {
+            startPosition = transform;
+        }
+    }
+}
